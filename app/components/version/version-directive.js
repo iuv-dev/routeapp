a6dba1c5382edbd72ca5b837041bbfe805b8f5b7
Clyde Shen <clyde.shen@outlook.com>
2016-04-03 14:52:22 +1200

    re-init with Angular seed

--- app/components/version/version-directive.js
+++ app/components/version/version-directive.js
@@ -3,7 +3,7 @@
 angular.module('myApp.version.version-directive', [])
 
 .directive('appVersion', ['version', function(version) {
-  return function(scope, elm) {
+  return function(scope, elm, attrs) {
     elm.text(version);
   };
 }]);
